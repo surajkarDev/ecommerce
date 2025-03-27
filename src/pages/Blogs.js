83418f697743074a69e7bda688595659828d5f99
suraj <suraj.kar@farenexus.com>
2025-03-27 10:50:13 +0530

    blogs add

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,4 +1,5 @@
 import React,{ useEffect, useState,useCallback,useMemo,memo,createContext,useRef } from "react";
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import { useNavigate } from "react-router-dom";
 import Blog from '../components/Blog'
 import axios from "axios";
@@ -14,6 +15,26 @@ const Blogs = () => {
   const ref = useRef();
   const [name ,setValue] = useCustomHook("username","")
   const [password ,setPassword] = useCustomHook("password","")
+  const blogPosts = [
+    {
+        id: 1,
+        title: 'The Latest Trends in Fashion',
+        description: 'Discover the newest fashion trends and styles for the season.',
+        image: 'https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?w=600&h=400&auto=compress&cs=tinysrgb',
+    },
+    {
+        id: 2,
+        title: 'How to Choose the Best Accessories',
+        description: 'Learn how to select accessories that match your outfit perfectly.',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRk7BtHfp2-qi8hpvhWMJ76AqtFcEDdCtdhxQ&s',
+    },
+    {
+        id: 3,
+        title: 'Sustainable Shopping Tips',
+        description: 'Find out how to shop sustainably and reduce environmental impact.',
+        image: 'https://images.pexels.com/photos/373564/pexels-photo-373564.jpeg?w=600&h=400&auto=compress&cs=tinysrgb',
+    }
+];
   // const newArray = new Array(30000000).fill(0).map((_,i) => {
   //   return {
   //     index:i,
@@ -163,7 +184,7 @@ const Blogs = () => {
         <PageHeader heading="Blogs" linkName="Blogs" link="/blogs" />
     </section>
     <section>
-      <div className="container">
+      <div className="container d-none">
         <div className="row">
           <div className="col-12">
             <counterContext.Provider value={count}>
@@ -183,6 +204,23 @@ const Blogs = () => {
             </div>
           </div>
         </div>
+        <Container className="mt-5">
+            {/* <h2 className="text-center mb-4">Latest Blog Posts</h2> */}
+            <Row>
+                {blogPosts.map(post => (
+                    <Col md={4} key={post.id} className="mb-4">
+                        <Card className="shadow-sm border-0 rounded">
+                            <Card.Img variant="top" src={post.image} alt={post.title} style={{ height: '200px', objectFit: 'cover' }} />
+                            <Card.Body>
+                                <Card.Title>{post.title}</Card.Title>
+                                <Card.Text>{post.description}</Card.Text>
+                                <Button variant="primary">Read More</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
     </section>
     </>;
   };
